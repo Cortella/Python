5b49a4aa6f12a99cfa0f2d36203d44d44dec7db2
Cortella <nicolau.cortella@gmail.com>
2020-02-09 05:14:21 -0300

    fim exemplificacao basica CONDICIONAIS

--- Teste/Doc.py
+++ Teste/Doc.py
@@ -19,6 +19,52 @@ if idade >= 18:
     print("voce ja pode dirigir")
 else:
     print("voce nao pode dirigir")
+    
+
+#IF COMPOSTO
+    if idade >=20:
+        if idade <=60:
+            print("IDADE ENTRE 20 E 60 ANOS")
+        else:
+            print("IDADE FORA DO PADRAO")
+    else:
+        print("IDADE FORA DO PADRAO")
+#IFS DEPENDENTES
+numero = 2
+if(numero == 1):
+    print("Segunda")
+if(numero == 2):
+    print("Terca")
+if(numero == 3):
+    print("Quarta")
+if(numero == 4):
+    print("Quinta")
+if(numero == 5):
+    print("Sexta")
+if(numero == 6):
+    print("Sabado")
+if(numero == 7):
+    print("Domingo")
+else:
+    print("Numero invalido")
+    
+#IFS INDEPENDENTES (DEPENDENTES APENAS DO PRIMEIRO IF)
+if(numero == 1):
+    print("Segunda")
+elif(numero == 2):
+    print("Terca")
+elif(numero == 3):
+    print("Quarta")
+elif(numero == 4):
+    print("Quinta")
+elif(numero == 5):
+    print("Sexta")
+elif(numero == 6):
+    print("Sabado")
+elif(numero == 7):
+    print("Domingo")
+else:
+    print("Numero invalido")
 #EXEMPLO DO USO DE INPUTS
 #INPUT E CASTING PARA INT AUTOMATICO
 #SISTEMA DE CALCULOS
