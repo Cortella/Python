6bf0cc9667bc426873bc9b9b569c08387e188a8d
Cortella <nicolau.cortella@gmail.com>
2020-02-09 03:50:35 -0300

    Inicio de Doc.py e exemplificacao de input

---
+++ Teste/Doc.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  9 03:02:56 2020
+
+@author: cortella
+"""
+
+#EXEMPLO DO USO DE INPUTS
+#INPUT E CASTING PARA INT AUTOMATICO
+#SISTEMA DE CALCULOS
+
+x = int(input("digite um valor"))
+y = int(input("digite um valor"))
+
+print("SOMA =",x+y)
