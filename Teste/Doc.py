c45fe36288fd12b7952e50a2e59a156a9d90c347
Cortella <nicolau.cortella@gmail.com>
2020-02-09 04:00:49 -0300

    Implementacao de estruturas condicionais basicas

--- Teste/Doc.py
+++ Teste/Doc.py
@@ -5,6 +5,20 @@ Created on Sun Feb  9 03:02:56 2020
 @author: cortella
 """
 
+#USO DE CONDICIONAIS 
+
+idade = int(input("Digite sua idade"))
+
+if idade >= 18:
+    print("voce ja pode dirigir")
+if idade < 18:
+    print("voce nao pode dirigir")
+    
+#USO DO ELSE
+if idade >= 18:
+    print("voce ja pode dirigir")
+else:
+    print("voce nao pode dirigir")
 #EXEMPLO DO USO DE INPUTS
 #INPUT E CASTING PARA INT AUTOMATICO
 #SISTEMA DE CALCULOS
